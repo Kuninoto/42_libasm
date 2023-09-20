219d32f0a21ab78006b24dcaa04728b1f15708ce
Kuninoto <miguelrebelo9999@gmail.com>
2023-09-20 17:29:37 +0100

    upd: ft_read

--- ft_write.s
+++ ft_write.s
@@ -7,7 +7,7 @@
 ;ssize_t write(int __fd, const void *__buf, size_t __n);
 ;rdi = fd
 ;rsi = buf
-;rdx = count
+;rdx = n
 ; refer to: http://blog.rchapman.org/posts/Linux_System_Call_Table_for_x86_64/
 
 BITS 64
@@ -16,27 +16,26 @@ extern __errno_location
 global ft_write
 
 ft_write:
-	push rbp ; Estabilish the stack frame
+	push rbp ; Estabilish stack frame
 	mov rbp, rsp ; Setup the base pointer
 
-	mov rax, 1 ; move 1 to rax, the write syscall number 
-	syscall ; call kernel
+	mov rax, 1 ; Move 1 to rax (write syscall number) 
+	syscall ; Call kernel
 
-	cmp rax, 0 ; compare the value in rax (return value of write) with 0
-	jl on_error ; jump if condition evaluated to less (in case of an error a negative value is returned)
+	cmp rax, 0 ; Compare the value in rax (return value) with 0
+	jl on_error ; Jump if condition evaluated to less (in case of an error a negative value is returned)
 
-	mov rbp, rsp ; tear down...
+	mov rbp, rsp ; Tear down...
 	pop rbp ; ...the stack frame
-	ret ; return to the caller
+	ret ; Return to the caller
 
-; review this
 on_error:
-	neg rax ; get the absolute value of syscall return
-    mov rdi, rax ; preserve syscall return value by saving it in rdi
-    call __errno_location WRT ..plt; get the address of errno on rax
-    mov [rax], rdi  ; set the value of errno onto the address of errno
-    mov rax, -1 ; move -1 to rax so that the caller receives -1 as the return value
+	neg rax ; Get the absolute value of the return value
+    mov rdi, rax ; Preserve return value by saving it on another register
+    call __errno_location WRT ..plt; Get the address of errno on rax
+    mov [rax], rdi  ; Dereference errno's address and set it to value of rdi (where we stored the absolute return value, which is our error number)
+    mov rax, -1 ; Move -1 to rax so that the caller receives -1 as the return value
 
-	mov rbp, rsp ; tear down...
+	mov rbp, rsp ; Tear down...
 	pop rbp ; ...the stack frame
-	ret ; return to the caller
+	ret ; Return to the caller
--- main.c
+++ main.c
@@ -2,22 +2,27 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
+#include <string.h>
+#include <memory.h>
+#include <fcntl.h>
 #include "./munit/munit.h"
 
 #define UNEXISTENT_FD 42
 
 ssize_t ft_write(int __fd, const void *__buf, ssize_t __n);
+ssize_t ft_read(int __fd, void *__buf, ssize_t __nbytes);
 
-int main(void) {
+int main(void)
+{
     printf("TESTING FT_WRITE\n");
     char nickname[] = "Kuninoto\n";
 
-    munit_log(MUNIT_LOG_INFO, "testing return values...\n");
+    munit_log(MUNIT_LOG_INFO, "testing return values...");
     ssize_t ft_write_ret = ft_write(STDOUT_FILENO, nickname, 9);
     ssize_t write_ret = write(STDOUT_FILENO, nickname, 9);
     munit_assert_long(ft_write_ret, ==, write_ret);
 
-    munit_log(MUNIT_LOG_INFO, "testing errno...\n");
+    munit_log(MUNIT_LOG_INFO, "testing errno...");
 
     // EBADF (9) - Bad file descriptor
     ft_write(UNEXISTENT_FD, nickname, 9);
@@ -29,7 +34,6 @@ int main(void) {
     printf("%s\n", strerror(write_errno));
 
     munit_assert_long(ft_write_errno, ==, write_errno);
-
     // EFAULT (14) - Bad Address
     ft_write(STDOUT_FILENO, NULL, 2);
     int ft_write_errno2 = errno;
@@ -40,6 +44,38 @@ int main(void) {
     printf("%s\n", strerror(write_errno2));
 
     munit_assert_long(ft_write_errno2, ==, write_errno2);
+
+    printf("TESTING FT_READ\n");
+    int file_fd = open("foo.txt", O_RDONLY);
+    char *ft_read_buffer = malloc(9 * sizeof(char));
+    char *read_buffer = malloc(9 * sizeof(char));
+
+    munit_log(MUNIT_LOG_INFO, "testing read strings && return values...");
+    ssize_t ft_read_ret = ft_read(file_fd, ft_read_buffer, 9);
+    lseek(file_fd, 0, SEEK_SET); // reset the file cursor
+    ssize_t read_ret = read(file_fd, read_buffer, 9);
+    munit_assert_string_equal(ft_read_buffer, read_buffer);
+    munit_assert_long(ft_read_ret, ==, read_ret);
+
+    memset(ft_read_buffer, 0, 9);
+    memset(read_buffer, 0, 9);
+    munit_log(MUNIT_LOG_INFO, "testing errno...\n");
+
+    // EBADF (9) - Bad file descriptor
+    ft_read(UNEXISTENT_FD, ft_read_buffer, 9);
+    int ft_read_errno = errno;
+    read(UNEXISTENT_FD, read_buffer, 9);
+    int read_errno = errno;
+    munit_assert_string_equal(strerror(ft_read_errno), strerror(read_errno));
+    munit_assert_long(ft_read_errno, ==, read_errno);
+
+    // EFAULT (14) - Bad Address
+    ft_read(file_fd, NULL, 9);
+    int ft_read_errno2 = errno;
+    read(file_fd, NULL, 9);
+    int read_errno2 = errno;
+    munit_assert_string_equal(strerror(ft_read_errno2), strerror(read_errno2));
+    munit_assert_long(ft_read_errno2, ==, read_errno2);
 
     return EXIT_SUCCESS;
 }
