3bee5ffc974d9c27a8a256dfb89635c7d9985059
Kuninoto <miguelrebelo9999@gmail.com>
2023-09-20 17:31:38 +0100

    upd: updated tests

--- main.c
+++ main.c
@@ -27,22 +27,17 @@ int main(void)
     // EBADF (9) - Bad file descriptor
     ft_write(UNEXISTENT_FD, nickname, 9);
     int ft_write_errno = errno;
-    printf("%s\n", strerror(ft_write_errno));
-
     write(UNEXISTENT_FD, nickname, 9);
     int write_errno = errno;
-    printf("%s\n", strerror(write_errno));
-
+    munit_assert_string_equal(strerror(ft_write_errno), strerror(write_errno));
     munit_assert_long(ft_write_errno, ==, write_errno);
+    
     // EFAULT (14) - Bad Address
     ft_write(STDOUT_FILENO, NULL, 2);
     int ft_write_errno2 = errno;
-    printf("%s\n", strerror(ft_write_errno2));
-
     write(STDOUT_FILENO, NULL, 2);
     int write_errno2 = errno;
-    printf("%s\n", strerror(write_errno2));
-
+    munit_assert_string_equal(strerror(ft_write_errno2), strerror(write_errno2));
     munit_assert_long(ft_write_errno2, ==, write_errno2);
 
     printf("TESTING FT_READ\n");
