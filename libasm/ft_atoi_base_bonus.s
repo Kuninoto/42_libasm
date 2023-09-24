fe11155fe739e7b2bc59e52e2a75fb721c0e4b4a
Kuninoto <miguelrebelo9999@gmail.com>
2023-09-24 22:05:13 +0100

    upd: comments

--- libasm/ft_atoi_base_bonus.s
+++ libasm/ft_atoi_base_bonus.s
@@ -12,6 +12,11 @@ section .text
         push rbp ; Estabilish stack frame
 	    mov rbp, rsp ; Setup the base pointer
     
+    is_valid_base:
+        ; Check if base is null
+        ; Check base's length 0 || 1
+        ; Check if it has white spaces or signs
+        ; Return 1 on error
 
     return:
         mov rbp, rsp ; Tear down...
