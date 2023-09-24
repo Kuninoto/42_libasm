84f9b8e72584c83733375762111f50ce426be852
Kuninoto <miguelrebelo9999@gmail.com>
2023-09-24 13:30:43 +0100

    upd: mandatory done

--- libasm/ft_strcmp.s
+++ libasm/ft_strcmp.s
@@ -1,4 +1,4 @@
-;char *strcpy(const char *__s1, const char *__s2)
+;char *strcmp(const char *__s1, const char *__s2)
 ;rdi = __s1
 ;rsi = __s2
 
@@ -10,13 +10,28 @@ ft_strcmp:
 	push rbp ; Estabilish stack frame
 	mov rbp, rsp ; Setup the base pointer
 
-	mov rcx, 0x00 ; Set rcx to 0. It'll be equivalent to an i (index) variable in C 
     jmp while ; Jump unconditionally to the while label
 
 while:
-	; Figure out how to do the && operator
+    cmp BYTE[rdi], 0x00 ; Compare the byte in rdi with 0 (to check if it is a null byte)
+    je return ; Jump if condition evaluates to true
+
+    cmp BYTE[rsi], 0x00 ; Compare the byte in rsi with 0 (to check if it is a null byte)
+    je return ; Jump if condition evaluates to true
+
+    mov ah, BYTE[rsi] ; Move the current char on __s2 to ah (serves as a temp variable)
+    cmp BYTE[rdi], ah ; Compare the current chars on both strings
+    jne return ; Jump if condition evaluates to false
+
+    inc rdi ; Increment rdi (our s1 ptr)
+    inc rsi ; Increment rsi (our s2 ptr)
+    jmp while ; Jump back to the while block
 
 return:
+    movzx rax, BYTE[rdi] ; Move the current character on __s1 to rax
+    movzx rbx, BYTE[rsi] ; Move the current charactere on __s2 to rbx
+    sub rax, rbx ; Subtract the last characters where the loop ended (s1[i] - s2[i])
+
     mov rbp, rsp ; Tear down...
     pop rbp ; ...the stack frame
     ret ; Return to the caller 
--- main.c
+++ main.c
@@ -10,11 +10,24 @@
 #define UNEXISTENT_FD 42
 
 size_t ft_strlen(const char *__s);
+char *ft_strcpy(char *dest, const char *src);
+int ft_strcmp(const char *__s1, const char *__s2);
 ssize_t ft_write(int __fd, const void *__buf, ssize_t __n);
 ssize_t ft_read(int __fd, void *__buf, ssize_t __nbytes);
-char *ft_strcpy(char *dest, const char *src);
+char *ft_strdup(const char *s);
 
-int main(void)
+void test_ft_strlen(void)
+{
+    printf("TESTING FT_STRLEN\n");
+    char str[] = "foo";
+
+    munit_log(MUNIT_LOG_INFO, "testing return values...");
+    size_t ft_strlen_ret = ft_strlen(str);
+    size_t strlen_ret = strlen(str);
+    munit_assert_ulong(ft_strlen_ret, ==, strlen_ret);
+};
+
+void test_ft_strcpy(void)
 {
     printf("TESTING FT_STRCPY\n");
     char s1_ft[] = "bar";
@@ -26,15 +39,36 @@ int main(void)
     char *ft_strcpy_ret = ft_strcpy(s1_ft, s2_ft);
     char *strcpy_ret = strcpy(s1_og, s2_og);
     munit_assert_string_equal(ft_strcpy_ret, strcpy_ret);
+}
 
-    /* printf("TESTING FT_STRLEN\n");
-    char foo[] = "bar";
+void test_ft_strcmp(void)
+{
+    printf("TESTING FT_STRCMP\n");
+    char s1[] = "bar";
+    char s2[] = "bar";
+
+    char s3[] = "bar";
+    char s4[] = "baz";
 
     munit_log(MUNIT_LOG_INFO, "testing return values...");
-    size_t ft_strlen_ret = ft_strlen(foo);
-    size_t strlen_ret = strlen(foo);
-    munit_assert_ulong(ft_strlen_ret, ==, strlen_ret);
+    int s1_s2_og_ret = strcmp(s1, s2);
+    int s3_s4_og_ret = strcmp(s3, s4);
 
+    int s1_s2_ft_ret = ft_strcmp(s1, s2);
+    int s3_s4_ft_ret = ft_strcmp(s3, s4);
+
+    printf("s1_s2_og_ret = %d\n", s1_s2_og_ret);
+    printf("s1_s2_ft_ret = %d\n", s1_s2_ft_ret);
+
+    printf("s3_s4_og_ret = %d\n", s3_s4_og_ret);
+    printf("s3_s4_ft_ret = %d\n", s3_s4_ft_ret);
+
+    munit_assert_int(s1_s2_og_ret, ==, s1_s2_ft_ret);
+    munit_assert_int(s3_s4_og_ret, ==, s3_s4_ft_ret);
+}
+
+void test_ft_write(void)
+{
     printf("TESTING FT_WRITE\n");
     char nickname[] = "Kuninoto\n";
 
@@ -60,7 +94,10 @@ int main(void)
     int write_errno2 = errno;
     munit_assert_string_equal(strerror(ft_write_errno2), strerror(write_errno2));
     munit_assert_long(ft_write_errno2, ==, write_errno2);
+}
 
+void test_ft_read(void)
+{
     printf("TESTING FT_READ\n");
     int file_fd = open("foo.txt", O_RDONLY);
     char *ft_read_buffer = malloc(9 * sizeof(char));
@@ -91,7 +128,38 @@ int main(void)
     read(file_fd, NULL, 9);
     int read_errno2 = errno;
     munit_assert_string_equal(strerror(ft_read_errno2), strerror(read_errno2));
-    munit_assert_long(ft_read_errno2, ==, read_errno2); */
+    munit_assert_long(ft_read_errno2, ==, read_errno2);
+}
 
+void test_ft_strdup(void)
+{
+    printf("TESTING FT_STRDUP\n");
+    char s1[] = "foo";
+    char s2[] = "bar";
+
+    munit_log(MUNIT_LOG_INFO, "testing return values...");
+    char *s1_og_ret = strdup(s1);
+    char *s1_ft_ret = ft_strdup(s1);
+
+    char *s2_og_ret = strdup(s2);
+    char *s2_ft_ret = ft_strdup(s2);
+
+    munit_assert_string_equal(s1_og_ret, s1_ft_ret);
+    munit_assert_string_equal(s2_og_ret, s2_ft_ret);
+
+    free(s1_og_ret);
+    free(s1_ft_ret);
+    free(s2_og_ret);
+    free(s2_ft_ret);
+}
+
+int main(void)
+{
+    test_ft_strlen();
+    test_ft_strcpy();
+    test_ft_strcmp();
+    test_ft_write();
+    test_ft_read();
+    test_ft_strdup();
     return EXIT_SUCCESS;
 }
