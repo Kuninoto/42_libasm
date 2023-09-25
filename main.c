6e95bd8991c85c8dde1a08b0d39563b9fae35e86
Nuno Miguel Nuno Carvalho <nnuno-ca@c1r14s8.42porto.com>
2023-09-25 16:10:35 +0100

    upd: modified test

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #include <fcntl.h>
 #include "./munit/munit.h"
 
-#define UNEXISTENT_FD 42
+#define UNEXISTENT_FD 231031
 
 size_t ft_strlen(const char *__s);
 char *ft_strcpy(char *dest, const char *src);
@@ -15,17 +15,23 @@ int ft_strcmp(const char *__s1, const char *__s2);
 ssize_t ft_write(int __fd, const void *__buf, ssize_t __n);
 ssize_t ft_read(int __fd, void *__buf, ssize_t __nbytes);
 char *ft_strdup(const char *s);
-int ft_atoi_base(char *str, char *base);
 
 void test_ft_strlen(void)
 {
     printf("TESTING FT_STRLEN\n");
     char str[] = "foo";
+    char empty[] = "";
 
     munit_log(MUNIT_LOG_INFO, "testing return values...");
-    size_t ft_strlen_ret = ft_strlen(str);
-    size_t strlen_ret = strlen(str);
-    munit_assert_ulong(ft_strlen_ret, ==, strlen_ret);
+    size_t ft_strlen_ret_1 = ft_strlen(str);
+    size_t strlen_ret_1 = strlen(str);
+
+    size_t ft_strlen_ret_2 = ft_strlen(empty);
+    size_t strlen_ret_2 = strlen(empty);
+
+    munit_assert_ulong(ft_strlen_ret_1, ==, strlen_ret_1);
+    munit_assert_ulong(ft_strlen_ret_2, ==, strlen_ret_2);
+    munit_assert_ulong(ft_strlen_ret_2, !=, strlen_ret_1);
 
     write(STDOUT_FILENO, "\n", 1);
 };
@@ -62,21 +68,30 @@ void test_ft_strcmp(void)
     char s3[] = "bar";
     char s4[] = "baz";
 
+    char s5[] = "";
+    char s6[] = "";
+
     munit_log(MUNIT_LOG_INFO, "testing return values...");
     int s1_s2_og_ret = strcmp(s1, s2);
     int s3_s4_og_ret = strcmp(s3, s4);
+    int s5_s6_og_ret = strcmp(s5, s6);
 
     int s1_s2_ft_ret = ft_strcmp(s1, s2);
     int s3_s4_ft_ret = ft_strcmp(s3, s4);
+    int s5_s6_ft_ret = ft_strcmp(s5, s6);
 
-    //printf("s1_s2_og_ret = %d\n", s1_s2_og_ret);
-    //printf("s1_s2_ft_ret = %d\n", s1_s2_ft_ret);
+    // printf("s1_s2_og_ret = %d\n", s1_s2_og_ret);
+    // printf("s1_s2_ft_ret = %d\n", s1_s2_ft_ret);
 
-    //printf("s3_s4_og_ret = %d\n", s3_s4_og_ret);
-    //printf("s3_s4_ft_ret = %d\n", s3_s4_ft_ret);
+    // printf("s3_s4_og_ret = %d\n", s3_s4_og_ret);
+    // printf("s3_s4_ft_ret = %d\n", s3_s4_ft_ret);
+
+    // printf("s5_s6_og_ret = %d\n", s5_s6_og_ret);
+    // printf("s5_s6_ft_ret = %d\n", s5_s6_ft_ret);
 
     munit_assert_int(s1_s2_og_ret, ==, s1_s2_ft_ret);
     munit_assert_int(s3_s4_og_ret, ==, s3_s4_ft_ret);
+    munit_assert_int(s5_s6_og_ret, ==, s5_s6_ft_ret);
 
     write(STDOUT_FILENO, "\n", 1);
 }
@@ -95,20 +110,26 @@ void test_ft_write(void)
     munit_log(MUNIT_LOG_INFO, "testing errno...");
 
     // EBADF (9) - Bad file descriptor
-    ft_write(UNEXISTENT_FD, nickname, 9);
+    int ft_write_bad_fd_ret = ft_write(UNEXISTENT_FD, nickname, 9);
     int ft_write_errno = errno;
-    write(UNEXISTENT_FD, nickname, 9);
+
+    int write_bad_fd_ret = write(UNEXISTENT_FD, nickname, 9);
     int write_errno = errno;
+
+    munit_assert_int(ft_write_errno, ==, write_errno);
     munit_assert_string_equal(strerror(ft_write_errno), strerror(write_errno));
-    munit_assert_long(ft_write_errno, ==, write_errno);
+    munit_assert_int(ft_write_bad_fd_ret, ==, write_bad_fd_ret);
 
     // EFAULT (14) - Bad Address
-    ft_write(STDOUT_FILENO, NULL, 2);
+    int ft_write_null_ret = ft_write(STDOUT_FILENO, NULL, 2);
     int ft_write_errno2 = errno;
-    write(STDOUT_FILENO, NULL, 2);
+
+    int write_null_ret = write(STDOUT_FILENO, NULL, 2);
     int write_errno2 = errno;
+
     munit_assert_string_equal(strerror(ft_write_errno2), strerror(write_errno2));
-    munit_assert_long(ft_write_errno2, ==, write_errno2);
+    munit_assert_int(ft_write_null_ret, ==, write_null_ret);
+    munit_assert_int(ft_write_errno2, ==, write_errno2);
 
     write(STDOUT_FILENO, "\n", 1);
 }
@@ -132,20 +153,29 @@ void test_ft_read(void)
     munit_log(MUNIT_LOG_INFO, "testing errno...");
 
     // EBADF (9) - Bad file descriptor
-    ft_read(UNEXISTENT_FD, ft_read_buffer, 9);
+    int ft_read_bad_fd_ret = ft_read(UNEXISTENT_FD, ft_read_buffer, 9);
     int ft_read_errno = errno;
-    read(UNEXISTENT_FD, read_buffer, 9);
+
+    int read_bad_fd_ret = read(UNEXISTENT_FD, read_buffer, 9);
     int read_errno = errno;
-    munit_assert_string_equal(strerror(ft_read_errno), strerror(read_errno));
+
     munit_assert_long(ft_read_errno, ==, read_errno);
+    munit_assert_string_equal(strerror(ft_read_errno), strerror(read_errno));
+    munit_assert_long(ft_read_bad_fd_ret, ==, read_bad_fd_ret);
 
     // EFAULT (14) - Bad Address
-    ft_read(file_fd, NULL, 9);
+    int ft_read_bad_fd_ret2 = ft_read(file_fd, NULL, 9);
     int ft_read_errno2 = errno;
-    read(file_fd, NULL, 9);
+
+    int read_bad_fd_ret2 = read(file_fd, NULL, 9);
     int read_errno2 = errno;
+
+    munit_assert_int(ft_read_errno2, ==, read_errno2);
     munit_assert_string_equal(strerror(ft_read_errno2), strerror(read_errno2));
-    munit_assert_long(ft_read_errno2, ==, read_errno2);
+    munit_assert_int(ft_read_bad_fd_ret2, ==, read_bad_fd_ret2);
+
+    free(ft_read_buffer);
+    free(read_buffer);
     write(STDOUT_FILENO, "\n", 1);
 }
 
@@ -154,6 +184,8 @@ void test_ft_strdup(void)
     printf("TESTING FT_STRDUP\n");
     char s1[] = "foo";
     char s2[] = "bar";
+    char s3[] = {129, '\0'};
+    char s4[] = "";
 
     munit_log(MUNIT_LOG_INFO, "testing return values...");
     char *s1_og_ret = strdup(s1);
@@ -162,13 +194,40 @@ void test_ft_strdup(void)
     char *s2_og_ret = strdup(s2);
     char *s2_ft_ret = ft_strdup(s2);
 
+    char *s3_og_ret = strdup(s3);
+    char *s3_ft_ret = ft_strdup(s3);
+
+    char *s4_og_ret = strdup(s4);
+    char *s4_ft_ret = ft_strdup(s4);
+
     munit_assert_string_equal(s1_og_ret, s1_ft_ret);
     munit_assert_string_equal(s2_og_ret, s2_ft_ret);
+    munit_assert_string_equal(s3_og_ret, s3_ft_ret);
+    munit_assert_string_equal(s4_og_ret, s4_ft_ret);
+
+    // printf("%s\n", s1_og_ret);
+    // printf("%s\n", s1_ft_ret);
+
+    // printf("%s\n", s2_og_ret);
+    // printf("%s\n", s2_ft_ret);
+
+    // printf("%s\n", s3_og_ret);
+    // printf("%s\n", s3_ft_ret);
+
+    //printf("%s\n", s4_og_ret);
+    //printf("%s\n", s4_ft_ret);
 
     free(s1_og_ret);
     free(s1_ft_ret);
+
     free(s2_og_ret);
     free(s2_ft_ret);
+
+    free(s3_og_ret);
+    free(s3_ft_ret);
+
+    free(s4_og_ret);
+    free(s4_ft_ret);
 }
 
 int main(void)
