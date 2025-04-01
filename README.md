35461b95041e2d9f647035f7ed06ff78e10d1ddd
Kuninoto <104518888+Kuninoto@users.noreply.github.com>
2025-04-02 00:39:09 +0100

    upd: README

---
+++ README.md
@@ -0,0 +1,50 @@
+# libasm (42Porto - 42Cursus)  
+
+## Description  
+
+A few string manipulation functions and libc system call wrappers written in Assembly x86_64 with the Intel syntax
+
+### Grade: 100/100  
+
+### Installing and running the project  
+1- Install dependencies  
+  
+	sudo apt install -y nasm
+2- Clone this repository  
+	
+	git clone https://github.com/Kuninoto/42_libasm
+3- Navigate to `libasm/`, assemble and wipe the artifact files  
+	
+	cd 42_libasm/libasm && make && make clean
+4- Compile your program with your new `libasm`
+
+	Example (with C): cc -Wall -Wextra -Werror main.c libasm.a
+
+#### Makefile available targets  
+`make` or `make all` - Makes `libasm.a`  
+`make clean` - Deletes all the resulting object files  
+`make fclean` - Deletes `libasm.a` and all the resulting object files  
+`make re` - Deletes everything (object files and `libasm.a`) and rebuilds `libasm.a`  
+
+#### Adding new functions to this library:
+  - Add their prototypes to `libasm.h`  
+  - Add their source files to `libasm/`  
+  - Add their source file names to the `GENERAL` variable inside the Makefile  
+  - Rebuild the library with `make re`  
+
+##### To keep track of what's inside your resulting `libasm.a` run `ar -t libasm.a` 
+
+## Useful links
+[You Can Learn Assembly in 10 Minutes (it’s easy)](https://www.youtube.com/watch?v=jPDiaZS-2ok)  
+[How do I know what registers function parameters will be mapped on?](https://stackoverflow.com/questions/28728742/how-do-i-know-what-registers-function-parameters-will-be-mapped-on)  
+[syscalls(2) man page](https://linux.die.net/man/2/syscalls)  
+[Linux System Call Table for x86_64](http://blog.rchapman.org/posts/Linux_System_Call_Table_for_x86_64/)  
+[x64 ASM Cheat Sheet](https://cs.brown.edu/courses/cs033/docs/guides/x64_cheatsheet.pdf)  
+[NASM tutorial](https://cs.lmu.edu/~ray/notes/nasmtutorial/)  
+[How does push and pop work in assembly](https://stackoverflow.com/questions/26026174/how-does-push-and-pop-work-in-assembly)  
+[CPU Registers x86](https://wiki.osdev.org/CPU_Registers_x86)  
+[Godbolt](https://godbolt.org/)  
+
+---
+Made by Nuno Carvalho (Kuninoto) | nnuno-ca@student.42porto.com  
+<div id="badge"> <a href="https://www.linkedin.com/in/nuno-carvalho-218822247"/> <img src="https://img.shields.io/badge/LinkedIn-blue?style=for-the-badge&logo=linkedin&logoColor=white" alt="LinkedIn Badge"/>&nbsp;
--- tests/main.c
+++ tests/main.c
@@ -5,124 +5,11 @@
 #include <string.h>
 #include <memory.h>
 #include <fcntl.h>
-#include "../munit/munit.h"
+
+#include "munit/munit.h"
 #include "libasm.h"
 
 #define UNEXISTENT_FD 231031
-
-void test_ft_strlen(void) {
-    printf("TESTING FT_STRLEN\n");
-    char str[] = "foo";
-    char empty[] = "";
-
-    munit_log(MUNIT_LOG_INFO, "testing return values...");
-    size_t ft_strlen_ret_1 = ft_strlen(str);
-    size_t strlen_ret_1 = strlen(str);
-
-    size_t ft_strlen_ret_2 = ft_strlen(empty);
-    size_t strlen_ret_2 = strlen(empty);
-
-    munit_assert_ulong(ft_strlen_ret_1, ==, strlen_ret_1);
-    munit_assert_ulong(ft_strlen_ret_2, ==, strlen_ret_2);
-    munit_assert_ulong(ft_strlen_ret_2, !=, strlen_ret_1);
-
-    write(STDOUT_FILENO, "\n", 1);
-};
-
-void test_ft_strcpy(void) {
-    printf("TESTING FT_STRCPY\n");
-    char s1_ft[] = "bar";
-    char s2_ft[] = "zzz";
-
-    char s1_og[] = "bar";
-    char s2_og[] = "zzz";
-
-    munit_log(MUNIT_LOG_INFO, "testing return values...");
-    char *ft_strcpy_ret = ft_strcpy(s1_ft, s2_ft);
-    char *strcpy_ret = strcpy(s1_og, s2_og);
-    munit_assert_string_equal(ft_strcpy_ret, strcpy_ret);
-
-    munit_assert_string_equal(s1_ft, s2_ft);
-    munit_assert_string_equal(s1_og, s2_og);
-
-    munit_assert_string_equal(s1_og, s1_ft);
-    munit_assert_string_equal(s2_og, s2_ft);
-
-    write(STDOUT_FILENO, "\n", 1);
-}
-
-void test_ft_strcmp(void) {
-    printf("TESTING FT_STRCMP\n");
-    char s1[] = "bar";
-    char s2[] = "bar";
-
-    char s3[] = "bar";
-    char s4[] = "baz";
-
-    char s5[] = "";
-    char s6[] = "";
-
-    munit_log(MUNIT_LOG_INFO, "testing return values...");
-    int s1_s2_og_ret = strcmp(s1, s2);
-    int s3_s4_og_ret = strcmp(s3, s4);
-    int s5_s6_og_ret = strcmp(s5, s6);
-
-    int s1_s2_ft_ret = ft_strcmp(s1, s2);
-    int s3_s4_ft_ret = ft_strcmp(s3, s4);
-    int s5_s6_ft_ret = ft_strcmp(s5, s6);
-
-    // printf("s1_s2_og_ret = %d\n", s1_s2_og_ret);
-    // printf("s1_s2_ft_ret = %d\n", s1_s2_ft_ret);
-
-    // printf("s3_s4_og_ret = %d\n", s3_s4_og_ret);
-    // printf("s3_s4_ft_ret = %d\n", s3_s4_ft_ret);
-
-    // printf("s5_s6_og_ret = %d\n", s5_s6_og_ret);
-    // printf("s5_s6_ft_ret = %d\n", s5_s6_ft_ret);
-
-    munit_assert_int(s1_s2_og_ret, ==, s1_s2_ft_ret);
-    munit_assert_int(s3_s4_og_ret, ==, s3_s4_ft_ret);
-    munit_assert_int(s5_s6_og_ret, ==, s5_s6_ft_ret);
-
-    write(STDOUT_FILENO, "\n", 1);
-}
-
-void test_ft_write(void) {
-    printf("TESTING FT_WRITE\n");
-    char nickname[] = "Kuninoto\n";
-
-    munit_log(MUNIT_LOG_INFO, "testing return values...");
-    ssize_t ft_write_ret = ft_write(STDOUT_FILENO, nickname, 9);
-    ssize_t write_ret = write(STDOUT_FILENO, nickname, 9);
-
-    munit_assert_long(ft_write_ret, ==, write_ret);
-
-    munit_log(MUNIT_LOG_INFO, "testing errno...");
-
-    // EBADF (9) - Bad file descriptor
-    int ft_write_bad_fd_ret = ft_write(UNEXISTENT_FD, nickname, 9);
-    int ft_write_errno = errno;
-
-    int write_bad_fd_ret = write(UNEXISTENT_FD, nickname, 9);
-    int write_errno = errno;
-
-    munit_assert_int(ft_write_errno, ==, write_errno);
-    munit_assert_string_equal(strerror(ft_write_errno), strerror(write_errno));
-    munit_assert_int(ft_write_bad_fd_ret, ==, write_bad_fd_ret);
-
-    // EFAULT (14) - Bad Address
-    int ft_write_null_ret = ft_write(STDOUT_FILENO, NULL, 2);
-    int ft_write_errno2 = errno;
-
-    int write_null_ret = write(STDOUT_FILENO, NULL, 2);
-    int write_errno2 = errno;
-
-    munit_assert_string_equal(strerror(ft_write_errno2), strerror(write_errno2));
-    munit_assert_int(ft_write_null_ret, ==, write_null_ret);
-    munit_assert_int(ft_write_errno2, ==, write_errno2);
-
-    write(STDOUT_FILENO, "\n", 1);
-}
 
 void test_ft_read(void) {
     printf("TESTING FT_READ\n");
@@ -165,6 +52,64 @@ void test_ft_read(void) {
 
     free(ft_read_buffer);
     free(read_buffer);
+    write(STDOUT_FILENO, "\n", 1);
+}
+
+void test_ft_strcmp(void) {
+    printf("TESTING FT_STRCMP\n");
+    char s1[] = "bar";
+    char s2[] = "bar";
+
+    char s3[] = "bar";
+    char s4[] = "baz";
+
+    char s5[] = "";
+    char s6[] = "";
+
+    munit_log(MUNIT_LOG_INFO, "testing return values...");
+    int s1_s2_og_ret = strcmp(s1, s2);
+    int s3_s4_og_ret = strcmp(s3, s4);
+    int s5_s6_og_ret = strcmp(s5, s6);
+
+    int s1_s2_ft_ret = ft_strcmp(s1, s2);
+    int s3_s4_ft_ret = ft_strcmp(s3, s4);
+    int s5_s6_ft_ret = ft_strcmp(s5, s6);
+
+    // printf("s1_s2_og_ret = %d\n", s1_s2_og_ret);
+    // printf("s1_s2_ft_ret = %d\n", s1_s2_ft_ret);
+
+    // printf("s3_s4_og_ret = %d\n", s3_s4_og_ret);
+    // printf("s3_s4_ft_ret = %d\n", s3_s4_ft_ret);
+
+    // printf("s5_s6_og_ret = %d\n", s5_s6_og_ret);
+    // printf("s5_s6_ft_ret = %d\n", s5_s6_ft_ret);
+
+    munit_assert_int(s1_s2_og_ret, ==, s1_s2_ft_ret);
+    munit_assert_int(s3_s4_og_ret, ==, s3_s4_ft_ret);
+    munit_assert_int(s5_s6_og_ret, ==, s5_s6_ft_ret);
+
+    write(STDOUT_FILENO, "\n", 1);
+}
+
+void test_ft_strcpy(void) {
+    printf("TESTING FT_STRCPY\n");
+    char s1_ft[] = "bar";
+    char s2_ft[] = "zzz";
+
+    char s1_og[] = "bar";
+    char s2_og[] = "zzz";
+
+    munit_log(MUNIT_LOG_INFO, "testing return values...");
+    char *ft_strcpy_ret = ft_strcpy(s1_ft, s2_ft);
+    char *strcpy_ret = strcpy(s1_og, s2_og);
+    munit_assert_string_equal(ft_strcpy_ret, strcpy_ret);
+
+    munit_assert_string_equal(s1_ft, s2_ft);
+    munit_assert_string_equal(s1_og, s2_og);
+
+    munit_assert_string_equal(s1_og, s1_ft);
+    munit_assert_string_equal(s2_og, s2_ft);
+
     write(STDOUT_FILENO, "\n", 1);
 }
 
@@ -218,12 +163,68 @@ void test_ft_strdup(void) {
     free(s4_ft_ret);
 }
 
+void test_ft_strlen(void) {
+    printf("TESTING FT_STRLEN\n");
+    char str[] = "foo";
+    char empty[] = "";
+
+    munit_log(MUNIT_LOG_INFO, "testing return values...");
+    size_t ft_strlen_ret_1 = ft_strlen(str);
+    size_t strlen_ret_1 = strlen(str);
+
+    size_t ft_strlen_ret_2 = ft_strlen(empty);
+    size_t strlen_ret_2 = strlen(empty);
+
+    munit_assert_ulong(ft_strlen_ret_1, ==, strlen_ret_1);
+    munit_assert_ulong(ft_strlen_ret_2, ==, strlen_ret_2);
+    munit_assert_ulong(ft_strlen_ret_2, !=, strlen_ret_1);
+
+    write(STDOUT_FILENO, "\n", 1);
+};
+
+void test_ft_write(void) {
+    printf("TESTING FT_WRITE\n");
+    char nickname[] = "Kuninoto\n";
+
+    munit_log(MUNIT_LOG_INFO, "testing return values...");
+    ssize_t ft_write_ret = ft_write(STDOUT_FILENO, nickname, 9);
+    ssize_t write_ret = write(STDOUT_FILENO, nickname, 9);
+
+    munit_assert_long(ft_write_ret, ==, write_ret);
+
+    munit_log(MUNIT_LOG_INFO, "testing errno...");
+
+    // EBADF (9) - Bad file descriptor
+    int ft_write_bad_fd_ret = ft_write(UNEXISTENT_FD, nickname, 9);
+    int ft_write_errno = errno;
+
+    int write_bad_fd_ret = write(UNEXISTENT_FD, nickname, 9);
+    int write_errno = errno;
+
+    munit_assert_int(ft_write_errno, ==, write_errno);
+    munit_assert_string_equal(strerror(ft_write_errno), strerror(write_errno));
+    munit_assert_int(ft_write_bad_fd_ret, ==, write_bad_fd_ret);
+
+    // EFAULT (14) - Bad Address
+    int ft_write_null_ret = ft_write(STDOUT_FILENO, NULL, 2);
+    int ft_write_errno2 = errno;
+
+    int write_null_ret = write(STDOUT_FILENO, NULL, 2);
+    int write_errno2 = errno;
+
+    munit_assert_string_equal(strerror(ft_write_errno2), strerror(write_errno2));
+    munit_assert_int(ft_write_null_ret, ==, write_null_ret);
+    munit_assert_int(ft_write_errno2, ==, write_errno2);
+
+    write(STDOUT_FILENO, "\n", 1);
+}
+
 int main(void) {
-    test_ft_strlen();
-    test_ft_strcpy();
-    test_ft_strcmp();
-    test_ft_write();
     test_ft_read();
+    test_ft_strcmp();
+    test_ft_strcpy();
     test_ft_strdup();
+    test_ft_strlen();
+    test_ft_write();
     return EXIT_SUCCESS;
 }
